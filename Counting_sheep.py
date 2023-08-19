3e1828ff33aaa39fb68df957a7f6af6d372620e1
Ivan Genik <IVGenik@rambler.ru>
2023-08-19 15:02:30 +0500

    7 task

---
+++ Counting_sheep.py
@@ -0,0 +1,20 @@
+'''
+Рассмотрим массив / список овец, в котором некоторые овцы могут отсутствовать на своем месте.
+Нам нужна функция, которая подсчитывает количество овец, присутствующих в массиве
+(true означает "присутствует").
+'''
+from functools import reduce
+
+def count_sheeps(sheep):
+    i=0
+    for j in sheep:
+        i += int(j)
+    return i
+
+def thetest_count_sheeps():
+    llst = [True,  True,  True,  False, True,  True,  True,  True ,  True,  False, True,  False,
+            True,  False, False, True ,  True,  True,  True,  True , False, False, True,  True]
+    print(count_sheeps(llst))
+
+if __name__ == "__main__":
+    thetest_count_sheeps()
