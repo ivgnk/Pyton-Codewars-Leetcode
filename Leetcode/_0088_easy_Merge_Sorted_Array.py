b46319489989957038973dc2e209dc5cfc02afaf
Ivan Genik <IVGenik@rambler.ru>
2024-03-12 14:52:06 +0500

    Add files via upload

---
+++ Leetcode/_0088_easy_Merge_Sorted_Array.py
@@ -0,0 +1,72 @@
+'''
+88. Merge Sorted Array
+https://leetcode.com/problems/merge-sorted-array/description/
+
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+
+To accommodate this, nums1 has a length of m + n,
+where the first m elements denote the elements that should be merged,
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Example 2:
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+Example 3:
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+'''
+
+
+def merge2(nums1, m, nums2, n):
+    """
+    :type nums1: List[int]
+    :type m: int
+    :type nums2: List[int]
+    :type n: int
+    :rtype: None Do not return anything, modify nums1 in-place instead.
+    """
+    if m == 0 and m == 0: return []
+    if m == 0: return nums2[0:n]
+    if n == 0: return nums1[0:m]
+    a = nums1[0:m] + nums2[0:n]
+    a.sort()
+    nums1 = a
+    return nums1
+
+def merge(nums1, m, nums2, n):
+    if m == 0 and n == 0:
+        nums1.clear()
+        return nums1
+    if m == 0:
+        nums1.clear()
+        nums1 = nums2[0:n]
+        nums1.sort()
+        return nums1
+
+    llen = len(nums1)
+    for i in range(m, m + n):
+        if i < llen:
+            nums1[i] = nums2[i - m-1]
+        else:
+            nums1.append(nums2[i - m-1])
+    nums1.sort()
+    return nums1
+
+# print(merge([1,2,3,0,0,0],3,[2,5,6],3))
+print(merge([0],0,[1],1))
