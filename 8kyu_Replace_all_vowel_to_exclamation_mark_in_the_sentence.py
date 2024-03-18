4f3f349f931e08c3ef147897d627b42120a9e4a8
Ivan Genik <IVGenik@rambler.ru>
2024-03-18 14:38:35 +0500

    Add files via upload

--- 8kyu_Replace_all_vowel_to_exclamation_mark_in_the_sentence.py
+++ 8kyu_Replace_all_vowel_to_exclamation_mark_in_the_sentence.py
@@ -5,5 +5,10 @@ def replace_exclamation(st):
         if s in vowel: st1[i] = "!"
     return ''.join(st1)
 
+# Bet of Codewars
+# def replace_exclamation(s):
+# 	for i in "aeuioAEUIO":	s = s.replace(i, "!")
+# 	return s
+
 print(replace_exclamation("Hi!"))
 print(replace_exclamation("ABCDE"))
