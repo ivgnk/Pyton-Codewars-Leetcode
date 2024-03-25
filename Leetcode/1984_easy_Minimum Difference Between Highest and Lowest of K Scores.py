0cd4bf097a46ae32ff0ba239647eac516504d585
Ivan Genik <IVGenik@rambler.ru>
2024-03-25 13:41:39 +0500

    Add files via upload

---
+++ Leetcode/1984_easy_Minimum Difference Between Highest and Lowest of K Scores.py
@@ -0,0 +1,46 @@
+'''
+1984. Minimum Difference Between Highest and Lowest of K Scores
+You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student.
+You are also given an integer k.
+Pick the scores of any k students from the array so that
+the difference between the highest and the lowest of the k scores is minimized.
+
+Return the minimum possible difference.
+
+Example 1:
+Input: nums = [90], k = 1
+Output: 0
+Explanation: There is one way to pick score(s) of one student:
+- [90]. The difference between the highest and lowest score is 90 - 90 = 0.
+The minimum possible difference is 0.
+
+Example 2:
+Input: nums = [9,4,1,7], k = 2
+Output: 2
+Explanation: There are six ways to pick score(s) of two students:
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 1 = 8.
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 7 = 2.
+- [9,4,1,7]. The difference between the highest and lowest score is 4 - 1 = 3.
+- [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
+- [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
+The minimum possible difference is 2.
+'''
+
+# Time Limit Exceeded  15 / 118 testcases passed
+
+
+def minimumDifference(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    import itertools
+    gldiff = int(1e38)
+    for comb in itertools.combinations(nums, k):
+        mmin = min(comb)
+        mmax = max(comb)
+        if mmax - mmin < gldiff:
+            gldiff = mmax - mmin
+    return gldiff
