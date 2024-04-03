e53c7fdd772a8b99c6c95657750b18829385ad5c
Ivan Genik <IVGenik@rambler.ru>
2024-04-03 14:39:41 +0500

    Add files via upload

---
+++ Leetcode/0191_easy_Number of 1 Bits.py
@@ -0,0 +1,28 @@
+'''
+191. Number of 1 Bits
+https://leetcode.com/problems/number-of-1-bits/description/
+
+Write a function that takes the binary representation of a positive integer and returns the number of
+set bits it has (also known as the Hamming weight).
+
+Example 1:
+Input: n = 11
+Output: 3
+Explanation: The input binary string 1011 has a total of three set bits.
+
+Example 2:
+Input: n = 128
+Output: 1
+Explanation: The input binary string 10000000 has a total of one set bit.
+'''
+
+# Runtime 7 ms Beats 97.34% of users with Python
+# Memory 11.51 MB Beats 70.39% of users with Python
+
+def hammingWeight(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    s = bin(n);  s = s[2:]
+    return sum([1 for s1 in s if s1 == '1'])
