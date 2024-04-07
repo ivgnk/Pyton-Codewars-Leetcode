906694271182a07db425a864f9a134ec0ca630fb
Ivan Genik <IVGenik@rambler.ru>
2024-04-07 16:01:48 +0500

    Add files via upload

---
+++ Leetcode/_1528_easy_Shuffle String.py
@@ -0,0 +1,31 @@
+'''
+1528. Shuffle String
+You are given a string s and an integer array indices of the same length.
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+Return the shuffled string.
+
+Example 1:
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+Example 2:
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+'''
+
+# Even 1 test case is incorrect
+
+def restoreString(s, indices):
+    """
+    :type s: str
+    :type indices: List[int]
+    :rtype: str
+    """
+    # for i in range(len(s)):
+    #     print(s[indices[i]])
+    s2 = ''.join([s[indices[i]] for i in range(len(s))])
+    return s2
+
+print('result=',restoreString(s = "codeleet", indices = [4,5,6,7,0,2,1,3]))
