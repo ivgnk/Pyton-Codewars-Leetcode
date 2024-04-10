ef6c8f9cc98cbe8ecf30bcbdc62bcbd16d8b2f48
Ivan Genik <IVGenik@rambler.ru>
2024-04-11 00:53:55 +0500

    Add files via upload

---
+++ 2413_easy_Smallest Even Multiple.py
@@ -0,0 +1,34 @@
+"""
+2413. Smallest Even Multiple
+https://leetcode.com/problems/smallest-even-multiple/description/
+
+Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+
+Example 1:
+Input: n = 5
+Output: 10
+Explanation: The smallest multiple of both 5 and 2 is 10.
+
+Example 2:
+Input: n = 6
+Output: 6
+Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
+"""
+
+# Runtime 8 ms Beats 88.95% of users with Python
+# Memory 11.65 MB Beats 37.42% of users with Python
+
+# Similar -  More challenges
+# 1071. Greatest Common Divisor of Strings
+# 1952. Three Divisors
+# 1979. Find Greatest Common Divisor of Array
+
+def smallestEvenMultiple(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    if n % 2 == 0:
+        return n
+    else:
+        return 2 * n
