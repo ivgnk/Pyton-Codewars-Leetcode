e3105c9abaeb2ef1d6a483b62a123e9e6ec28bc3
Ivan Genik <IVGenik@rambler.ru>
2024-04-21 23:24:57 +0500

    Add files via upload

---
+++ Leetcode/0709_easy_To Lower Case.py
@@ -0,0 +1,28 @@
+"""
+709. To Lower Case
+https://leetcode.com/problems/to-lower-case/description/
+
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Example 2:
+Input: s = "here"
+Output: "here"
+
+Example 3:
+Input: s = "LOVELY"
+Output: "lovely"
+"""
+
+# Runtime 7 ms Beats 94.58% of users with Python
+# Memory 11.75 MB Beats 24.67% of users with Python
+
+def toLowerCase(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    return s.lower()
