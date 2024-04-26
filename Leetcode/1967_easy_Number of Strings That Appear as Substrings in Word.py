8f95e703c03434d8ed3b8f36e6a9bc8f1f587f30
Ivan Genik <IVGenik@rambler.ru>
2024-04-26 18:10:36 +0500

    Add files via upload

---
+++ Leetcode/1967_easy_Number of Strings That Appear as Substrings in Word.py
@@ -0,0 +1,44 @@
+"""
+1967. Number of Strings That Appear as Substrings in Word
+https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/description/
+
+iven an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+Input: patterns = ["a","abc","bc","d"], word = "abc"
+Output: 3
+Explanation:
+- "a" appears as a substring in "abc".
+- "abc" appears as a substring in "abc".
+- "bc" appears as a substring in "abc".
+- "d" does not appear as a substring in "abc".
+3 of the strings in patterns appear as a substring in word.
+
+Example 2:
+Input: patterns = ["a","b","c"], word = "aaaaabbbbb"
+Output: 2
+Explanation:
+- "a" appears as a substring in "aaaaabbbbb".
+- "b" appears as a substring in "aaaaabbbbb".
+- "c" does not appear as a substring in "aaaaabbbbb".
+2 of the strings in patterns appear as a substring in word.
+
+Example 3:
+Input: patterns = ["a","a","a"], word = "ab"
+Output: 3
+Explanation: Each of the patterns appears as a substring in word "ab".
+"""
+
+# Runtime 15 ms Beats 77.78% of users with Python
+# Memory 11.95 MB Beats 9.88% of users with Python
+
+def numOfStrings(patterns, word):
+    """
+    :type patterns: List[str]
+    :type word: str
+    :rtype: int
+    """
+    return sum(1 for i in patterns if word.find(i)>-1)
+
+print(numOfStrings(patterns = ["a","abc","bc","d"], word ="abc"))
