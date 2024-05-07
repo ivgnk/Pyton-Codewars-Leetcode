d2137267ed071fbc7e9328301090316ada0119e4
Ivan Genik <IVGenik@rambler.ru>
2024-05-07 13:32:42 +0500

    Done 07.05.2024

---
+++ Leetcode/2828_easy_Check if a String Is an Acronym of Words.py
@@ -0,0 +1,47 @@
+"""
+Done 07.05.2024
+2828. Check if a String Is an Acronym of Words
+https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/description/
+
+Given an array of strings words and a string s, determine if s is an acronym of words.
+The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+Return true if s is an acronym of words, and false otherwise.
+
+Example 1:
+Input: words = ["alice","bob","charlie"], s = "abc"
+Output: true
+Explanation: The first character in the words "alice", "bob", and "charlie" are 'a', 'b', and 'c', respectively. Hence, s = "abc" is the acronym.
+
+Example 2:
+Input: words = ["an","apple"], s = "a"
+Output: false
+Explanation: The first character in the words "an" and "apple" are 'a' and 'a', respectively.
+The acronym formed by concatenating these characters is "aa".
+Hence, s = "a" is not the acronym.
+
+Example 3:
+Input: words = ["never","gonna","give","up","on","you"], s = "ngguoy"
+Output: true
+Explanation: By concatenating the first character of the words in the array, we get the string "ngguoy".
+Hence, s = "ngguoy" is the acronym.
+"""
+
+# Runtime 29 ms Beats 64.34% of users with Python
+# Memory 11.64 MB Beats 72.54% of users with Python
+# Runtime 22 ms Beats 95.29% of users with Python
+# Memory 11.83 MB Beats 10.86% of users with Python
+
+def isAcronym(words, s):
+    """
+    :type words: List[str]
+    :type s: str
+    :rtype: bool
+    """
+    if len(s) == len(words):
+        return all([True if s[i]==w[0] else False for i,w in enumerate(words) ])
+    else: return False
+
+print(isAcronym(words = ["alice","bob","charlie"], s = "abc"))
+print(isAcronym(words = ["an","apple"], s = "a"))
+print(isAcronym(words = ["never","gonna","give","up","on","you"], s = "ngguoy"))
+
