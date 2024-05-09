5e39bfff2b575463fd57e91c35f19087a39f50d4
Ivan Genik <IVGenik@rambler.ru>
2024-05-09 14:37:56 +0500

    Done 09.05.2024. Hash Table, Sorting, Enumeration

---
+++ Leetcode/___2094_easy_Finding 3-Digit Even Numbers.py
@@ -0,0 +1,51 @@
+"""
+Done 09.05.2024. Topics: Array, Hash Table, Sorting, Enumeration
+2094. Finding 3-Digit Even Numbers
+https://leetcode.com/problems/finding-3-digit-even-numbers/
+
+You are given an integer array digits, where each element is a digit.
+The array may contain duplicates.
+You need to find all the unique integers that follow the given requirements:
+
+The integer consists of the concatenation of three elements from digits in any arbitrary order.
+The integer does not have leading zeros.
+The integer is even.
+For example, if the given digits were [1, 2, 3], integers 132 and 312 follow the requirements.
+
+Return a sorted array of the unique integers.
+
+Example 1:
+Input: digits = [2,1,3,0]
+Output: [102,120,130,132,210,230,302,310,312,320]
+Explanation: All the possible integers that follow the requirements are in the output array.
+Notice that there are no odd integers or integers with leading zeros.
+
+Example 2:
+Input: digits = [2,2,8,8,2]
+Output: [222,228,282,288,822,828,882]
+Explanation: The same digit can be used as many times as it appears in digits.
+In this example, the digit 8 is used twice each time in 288, 828, and 882.
+
+Example 3:
+Input: digits = [3,7,5]
+Output: []
+Explanation: No even integers can be formed using the given digits.
+"""
+
+# Runtime 831 ms Beats 23.81% of users with Python
+# Memory 11.76 MB Beats 38.10% of users with Python
+
+from itertools import permutations
+def findEvenNumbers(digits):
+    """
+    :type digits: List[int]
+    :rtype: List[int]
+    """
+    perm = list(set(permutations(digits,3)))
+    p2= [int(str(d[0])+str(d[1])+str(d[2])) for d in perm]
+    p3 =sorted([d for d in p2 if len(str(d))==3 and d%2 == 0])
+    return p3
+
+print(findEvenNumbers([2,1,3,0]))
+
+
