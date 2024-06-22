e8454fc7ab7cf65f9ddd15dfa8cf4373152dc53b
Ivan Genik <IVGenik@rambler.ru>
2024-06-23 00:03:04 +0500

    Done 23.06.2024. Topics: Array, Hash Table

---
+++ Leetcode/2225_medi_Find Players With Zero or One Losses.py
@@ -0,0 +1,72 @@
+"""
+Done 23.06.2024. Topics: Array, Hash Table
+Topics: Array, Hash Table, Sorting, Counting
+
+2225. Find Players With Zero or One Losses
+https://leetcode.com/problems/find-players-with-zero-or-one-losses/description/
+
+You are given an integer array matches where matches[i] = [winneri, loseri]
+indicates that the player winneri defeated player loseri in a match.
+Return a list answer of size 2 where:
+answer[0] is a list of all players that have not lost any matches.
+answer[1] is a list of all players that have lost exactly one match.
+The values in the two lists should be returned in increasing order.
+Note:
+You should only consider the players that have played at least one match.
+The testcases will be generated such that no two matches will have the same outcome.
+
+Example 1:
+Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+Output: [[1,2,10],[4,5,7,8]]
+Explanation:
+Players 1, 2, and 10 have not lost any matches.
+Players 4, 5, 7, and 8 each have lost one match.
+Players 3, 6, and 9 each have lost two matches.
+Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+
+Example 2:
+Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+Output: [[1,2,5,6],[]]
+Explanation:
+Players 1, 2, 5, and 6 have not lost any matches.
+Players 3 and 4 each have lost two matches.
+Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+
+Hint 1
+Count the number of times a player loses while iterating through the matches.
+"""
+
+# Runtime 1433 ms Beats 28.45%
+# Memory 71.22 MB Beats 46.75%
+
+def findWinners(matches):
+    """
+    :type matches: List[List[int]]
+    :rtype: List[List[int]]
+    """
+    dd=dict()
+    for i in range(len(matches)):
+        win,los = matches[i]
+        if win in dd:
+            dd[win][0]=dd[win][0]+1
+        else:
+            dd[win]=[1,0]
+
+        if los in dd:
+            dd[los][1]=dd[los][1]+1
+        else:
+            dd[los]=[0,1]
+
+    wi=[]
+    lo=[]
+    for k,v in dd.items():
+        if v[1]==0:
+            wi.append(k)
+        elif v[1]==1:
+            lo.append(k)
+    return [sorted(wi),sorted(lo)]
+
+print(findWinners([[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]))
+
+
+
