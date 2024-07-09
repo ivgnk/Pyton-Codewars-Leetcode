a7a8aa1873b02b088779e431e4f255f469703c57
Ivan Genik <IVGenik@rambler.ru>
2024-07-09 10:27:48 +0500

    Done 09.07.2024. Topics: Linked List, Math

--- Leetcode/linkedlist_0002_medi_Add Two Numbers.py
+++ Leetcode/linkedlist_0002_medi_Add Two Numbers.py
@@ -1,5 +1,5 @@
 """
-09.07.2024. Topics: Linked List, Math
+Done 09.07.2024. Topics: Linked List, Math
 You are given two non-empty linked lists representing two non-negative integers.
 The digits are stored in reverse order, and each of their nodes contains a single digit.
 Add the two numbers and return the sum as a linked list.
