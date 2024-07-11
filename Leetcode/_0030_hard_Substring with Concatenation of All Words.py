16c3190e2678271ade16d1b1418e406be319a6e4
Ivan Genik <IVGenik@rambler.ru>
2024-07-11 12:48:29 +0500

    11.07.2024. Hash Table, String, Sliding Window

---
+++ Leetcode/_0030_hard_Substring with Concatenation of All Words.py
@@ -0,0 +1,49 @@
+"""
+
+"""
+from icecream import ic
+from itertools import permutations
+
+# Memory Limit Exceeded - 151 / 181 testcases passed
+def findSubstring(s, words):
+    """
+    :type s: str
+    :type words: List[str]
+    :rtype: List[int]
+    """
+    ll=len(s); rll=range(ll)
+
+    lst=list(permutations(words))
+    lst=list({''.join(prm) for prm in lst})
+    res=[]
+
+    sset = set()
+    for ss in lst:
+        if s.count(ss)>0:
+            n=0
+            while True:
+                n=s.find(ss, n)
+                if n > -1:
+                    sset.add(n)
+                    n=n+1
+                else:
+                    break
+    res = res + list(sset)
+
+
+    return list(set(res))
+
+# print(findSubstring( s = "barfoothefoobarman", words = ["foo","bar"]))
+# print(findSubstring(s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]))
+# print(findSubstring(s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]))
+# ic(findSubstring(s = "abababab", words = ["a","b","a"]))
+# ic(findSubstring(s = "mississippi", words = ["si","is"]))
+
+s = "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel"
+words = ["dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"]
+# for i in range(len(words)):
+#     print(i,s.count(words[i]))
+print(len(s))
+for i in range(2,len(words)):
+    print(i,s.count(words[i]+words[i-1]+words[i-2]))
+    print(i,s.count(words[i-2]+words[i-1]+words[i]))
