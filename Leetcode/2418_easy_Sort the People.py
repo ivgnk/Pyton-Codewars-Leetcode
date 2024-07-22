7846f39fe5138bde990da5c3d3bf59e94df914db
Ivan Genik <IVGenik@rambler.ru>
2024-07-22 13:08:03 +0500

    Redone 22.07.2024. Topics: Array, Sorting

--- Leetcode/2418_easy_Sort the People.py
+++ Leetcode/2418_easy_Sort the People.py
@@ -1,4 +1,5 @@
 """
+Done 26.04.2024. Redone 22.07.2024. Topics: Array, Sorting
 You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
 For each index i, names[i] and heights[i] denote the name and height of the ith person.
 Return names sorted in descending order by the people's heights.
@@ -14,9 +15,17 @@ Output: ["Bob","Alice","Bob"]
 Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
 """
 
+# Runtime 75 ms Beats 87.02%
+# Memory 12.18 MB Beats 61.97%
+def sortPeople(names, heights):
+    rll = range(len(names))
+    tmp = [(heights[i], names[i]) for i in rll]
+    tmp.sort(reverse=True)
+    return [tmp[i][1] for i in rll]
+
+
 # Runtime 76 ms Beats 86.15% of users with Python
 # Memory 12.38 MB Beats 14.04% of users with Python
-
 def sortPeople(names, heights):
     """
     :type names: List[str]
