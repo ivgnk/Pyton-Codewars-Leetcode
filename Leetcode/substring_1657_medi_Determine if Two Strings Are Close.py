8420c8d40f20c4be1a3071b7ac388753927a7502
Ivan Genik <IVGenik@rambler.ru>
2024-07-23 11:33:37 +0500

    Done 23.07.2024. Hash Table, String, Sorting

---
+++ Leetcode/substring_1657_medi_Determine if Two Strings Are Close.py
@@ -0,0 +1,73 @@
+"""
+Done 23.07.2024. Topics: Hash Table, String, Sorting
+1657. Determine if Two Strings Are Close
+https://leetcode.com/problems/determine-if-two-strings-are-close
+
+Two strings are considered close if you can attain one from the other using the following operations:
+Operation 1: Swap any two existing characters.
+For example, abcde -> aecdb
+Operation 2: Transform every occurrence of one existing character into another existing character,
+and do the same with the other character.
+For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+You can use the operations on either string as many times as necessary.
+Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+Example 1:
+Input: word1 = "abc", word2 = "bca"
+Output: true
+Explanation: You can attain word2 from word1 in 2 operations.
+Apply Operation 1: "abc" -> "acb"
+Apply Operation 1: "acb" -> "bca"
+
+Example 2:
+Input: word1 = "a", word2 = "aa"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+
+Example 3:
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+
+Constraints:
+1 <= word1.length, word2.length <= 10**5
+word1 and word2 contain only lowercase English letters.
+
+Hint 1
+Operation 1 allows you to freely reorder the string.
+Hint 2
+Operation 2 allows you to freely reassign the letters' frequencies.
+"""
+
+# 23.07.2024
+# Runtime 229 ms Beats 66.31%
+# Memory 15.22 MB Beats 16.22%
+def closeStrings(word1, word2):
+    """
+    :type word1: str
+    :type word2: str
+    :rtype: bool
+    """
+    if len(word1) != len(word2):  return False
+    w1=sorted(word1);  w2=sorted(word2)
+    print(w1)
+    print(w2)
+    if  w1== w2: return True
+    dd1=dict(); dd2=dict()
+    for w in word1:
+        if w in dd1: dd1[w]=dd1[w]+1
+        else: dd1[w]=1
+    for w in word2:
+        if w in dd2: dd2[w]=dd2[w]+1
+        else: dd2[w]=1
+    return sorted([v for v in dd1.values()])==sorted([v for v in dd2.values()])
+
+# print(closeStrings(word1 = "abc", word2 = "bca")) # True
+# print(closeStrings(word1 = "a", word2 = "aa")) # False
+print(closeStrings(word1 = "cabbba", word2 = "abbccc")) # True
+
+print(closeStrings(word1 = "uau" , word2 = "ssx")) # False
+
