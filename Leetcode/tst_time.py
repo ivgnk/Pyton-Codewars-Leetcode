3ddd733b232a53efb72f4418827ca6b3a1ac86e6
Ivan Genik <IVGenik@rambler.ru>
2024-07-30 10:10:26 +0500

    30.07.2024. Тестирование времени циклов

---
+++ Leetcode/tst_time.py
@@ -0,0 +1,35 @@
+"""
+30.07.2024. Тестирование времени циклов
+https://thecode.media/python-timing/
+"""
+import datetime
+import random
+
+n=100000
+s=[0 for i in range(n)]
+###---------- 1
+b=0
+start = datetime.datetime.now()
+for i in range(n):
+    b=s[i]
+finish = datetime.datetime.now()
+print('for i in range(n): ',finish - start)
+
+###---------- 2
+b=0
+start = datetime.datetime.now()
+i=0
+while i<=n-1:
+    b=s[i]
+    i=i+1
+finish = datetime.datetime.now()
+print('     while i<=n-1: ',finish - start)
+
+###---------- 3
+b=0
+start = datetime.datetime.now()
+for s1 in s:
+    b=s1
+finish = datetime.datetime.now()
+print('      for s1 in s: ',finish - start)
+
