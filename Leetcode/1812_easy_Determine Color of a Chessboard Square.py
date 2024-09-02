6bd7c78e7074baf7e2d0a79ca8405c9d62db9a78
Ivan Genik <IVGenik@rambler.ru>
2024-09-02 12:13:13 +0500

    ReDone 02.09.2024

--- Leetcode/1812_easy_Determine Color of a Chessboard Square.py
+++ Leetcode/1812_easy_Determine Color of a Chessboard Square.py
@@ -1,4 +1,5 @@
 '''
+ReDone 02.09.2024.
 1812. Determine Color of a Chessboard Square
 https://leetcode.com/problems/determine-color-of-a-chessboard-square/description/
 
@@ -6,12 +7,46 @@ You are given coordinates, a string that represents the coordinates of a square 
 Return true if the square is white, and false if the square is black.
 The coordinate will always represent a valid chessboard square.
 The coordinate will always have the letter first, and the number second.
+
+Example 1:
+Input: coordinates = "a1"
+Output: false
+Explanation: From the chessboard above, the square with coordinates "a1" is black, so return false.
+
+Example 2:
+Input: coordinates = "h3"
+Output: true
+Explanation: From the chessboard above, the square with coordinates "h3" is white, so return true.
+
+Example 3:
+Input: coordinates = "c7"
+Output: false
+
+Constraints:
+coordinates.length == 2
+'a' <= coordinates[0] <= 'h'
+'1' <= coordinates[1] <= '8'
+
+Hint 1
+Convert the coordinates to (x, y) - that is, "a1" is (1, 1), "d7" is (4, 7).
+Hint 2
+Try add the numbers together and look for a pattern.
+
+Hint 1 from 3274. Check if Two Chessboard Squares Have the Same Color
+The color of the chessboard is black the sum of row coordinates and column coordinates is even.
+Otherwise, it's white.
 '''
+
+# Runtime 12 ms Beats 66.40%
+# Memory 11.63 MB Beats 65.60%
+
+w={'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7}
+def squareIsWhite(coo):
+    return (w[coo[0]] + int(coo[1])) %2 == 0
 
 # Runtime 12 ms Beats 66.30% of users with Python
 # Memory 11.82 MB Beats 5.52% of users with Python
-
-def squareIsWhite(self, coordinates):
+def squareIsWhite2(coordinates):
     """
     :type coordinates: str
     :rtype: bool
@@ -22,3 +57,4 @@ def squareIsWhite(self, coordinates):
     if int(coordinates[1]) % 2 == 0:
         return l2[n]
     else: return l1[n]
+
