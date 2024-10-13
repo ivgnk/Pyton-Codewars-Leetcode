b1f7fde4332687327371bb4fa1982213e393d3cd
Ivan Genik <IVGenik@rambler.ru>
2024-10-13 14:47:29 +0500

    Done 13.10.2024. Topics: Sorting

---
+++ Leetcode/LOCKED_1086_easy_High Five.py
@@ -0,0 +1,54 @@
+"""
+Done 13.10.2024. Topics: Sorting
+1086. High Five
+https://leetcode.ca/2018-11-20-1086-High-Five/
+https://leetcode.com/problems/high-five/
+
+Description
+Given a list of the scores of different students, items, where items[i] = [IDi, scorei]
+represents one score from a student with IDi, calculate each student's top five average.
+
+Return the answer as an array of pairs result, where result[j] = [IDj, topFiveAveragej]
+represents the student with IDj and their top five average. Sort result by IDj in increasing order.
+
+A student's top five average is calculated by taking the sum of their top five scores and dividing
+it by 5 using integer division.
+
+Example 1:
+Input: items = [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+Output: [[1,87],[2,88]]
+Explanation:
+The student with ID = 1 got scores 91, 92, 60, 65, 87, and 100.
+Their top five average is (100 + 92 + 91 + 87 + 65) / 5 = 87.
+The student with ID = 2 got scores 93, 97, 77, 100, and 76.
+Their top five average is (100 + 97 + 93 + 77 + 76) / 5 = 88.6,
+but with integer division their average converts to 88.
+
+Example 2:
+Input: items = [[1,100],[7,100],[1,100],[7,100],[1,100],[7,100],[1,100],[7,100],[1,100],[7,100]]
+Output: [[1,100],[7,100]]
+
+Constraints:
+1 <= items.length <= 1000
+items[i].length == 2
+1 <= IDi <= 1000
+0 <= scorei <= 100
+For each IDi, there will be at least five scores.
+"""
+
+from  collections import  defaultdict
+# items[i] = [IDi, scorei]
+# from statistics import mean
+def highFive(items):
+    dd=defaultdict(list)
+    res=[]
+    for id,score in items:
+        dd[id].append(score)
+    # print(dd)
+    for k,v in dd.items():
+        z=sum(sorted(v,reverse=True)[0:5])
+        res.append([k, z//5])
+    return res
+
+print(highFive([[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]])) # [[1,87],[2,88]]
+print(highFive([[1,100],[7,100],[1,100],[7,100],[1,100],[7,100],[1,100],[7,100],[1,100],[7,100]]))
